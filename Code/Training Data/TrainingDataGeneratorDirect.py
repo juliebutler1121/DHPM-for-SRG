d38255ca7f5c08361e603443137f0b4128973f6f
Julie Butler <butler@frib.msu.edu>
2019-02-09 17:47:57 -0500

    Code completed, still needs to be documented and commented

--- Code/Training Data/TrainingDataGeneratorDirect.py
+++ Code/Training Data/TrainingDataGeneratorDirect.py
@@ -3,3 +3,37 @@
 # Julie Butler
 # 2-5-19
 #########################################################################################
+
+from math import sin, pi
+from numpy import arange
+
+def wave_equation (nx, ny, nz, L, x, y, z):
+    return (2/L)**(3/2)*sin(nx*pi*x/L)*sin(ny*pi*y/L)*sin(nz*pi*z/L)
+
+def main ():
+    nx = 1
+    ny = 1
+    nz = 1
+
+    L = 1
+
+    space_values = arange (0, L, 1e-2)
+
+    F = open("Schrodinger_Equation_Training_Data_1_e_-2.txt", 'w')
+
+    for x in space_values:
+        for y in space_values:
+            for z in space_values:
+                to_file = str(x) + "," + str(y) + "," + str(z) + ","
+                wave_value = wave_equation (nx, ny, nx, L, x, y, z)
+                to_file = to_file + str(wave_value) + "\n"
+                F.write(to_file)
+
+#------------------------------------------------------------------------------
+# make executable
+#------------------------------------------------------------------------------
+if __name__ == "__main__": 
+  main()
+
+
+                
