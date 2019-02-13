2a1665da81a01f81689dcb741e73c0011d288495
Julie Butler <butler@frib.msu.edu>
2019-02-13 10:10:43 -0500

    added class structure

--- Code/Neural Networks/ProofOfConcept.py
+++ Code/Neural Networks/ProofOfConcept.py
@@ -1,4 +1,22 @@
 import tensorflow as tf
 import numpy as np
+from NeuralNetworkFunctions import initialize_neural_network
 
-def initialize (x, y, z, exact, X_f, layers, lower_bound, upper_bound):
+
+class ProofOfConceptNeuralNetwork:
+    def _init_ (self, x, y, z, exact, X_f, layers, lower_bound, upper_bound):
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.exact = exact
+
+        self.X_f = X_f
+
+        self.layers = layers
+
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+                
+        self.weights, self.biases = initialize_neural_network (layers)
