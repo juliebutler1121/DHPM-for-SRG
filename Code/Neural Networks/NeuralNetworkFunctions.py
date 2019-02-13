7d59b08973f2d8c40a7029c4c858ddb72562e2f3
Julie Butler <butler@frib.msu.edu>
2019-02-13 09:58:15 -0500

    added xavier initialization

--- Code/Neural Networks/NeuralNetworkFunctions.py
+++ Code/Neural Networks/NeuralNetworkFunctions.py
@@ -8,5 +8,17 @@ def initialize_neural_network (layers):
     number_of_layers = len(layers)
 
     for l in range (0, number_of_layers-1):
-        w = xavier_initilization([layers[l], layers[l+1]])
-        b = tf.Variable (tf.
+        w = xavier_initilization(layers[l], layers[l+1])
+        b = tf.Variable (tf.zeros([1, layers[l+1]]))
+
+        weights.append (w)
+        biases.append (b)
+
+    return weights, biases
+
+def xavier_initilization (layer_in, layer_out):
+    xavier_standard_deviation = np.sqrt(2/(layer_in + layer_out))
+
+    return tf.Variable (tf.truncated_normal([layer_in, layer_out], 
+        stddev=xavier_standard_deviation))
+    
