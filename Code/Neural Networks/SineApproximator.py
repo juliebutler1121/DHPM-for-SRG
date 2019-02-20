710a6e713ba96eca7ba9b78d98df7611ca8259eb
Julie Butler <butler@frib.msu.edu>
2019-02-20 09:44:31 -0500

    restructured to remove code which may be used again.  placed common code in NeuralNetworkFunctions

--- Code/Neural Networks/SineApproximator.py
+++ Code/Neural Networks/SineApproximator.py
@@ -1,25 +1,36 @@
+#########################################################################################
+# SineApproximator.py
+# Julie Butler
+# February 20, 2019
+# Verision 0.1
+#
+# Approximates the function sin(x) using a one hidden layer neural network with any
+# number of neurons.  In general, as the number of neurons increases, the accuracy
+# of the approximation increases
+#########################################################################################
+
+#############################
+#                           #
+# IMPORTS                   #
+#                           #
+#############################
+# THIRD-PARTY IMPORTS
+# For machine learning
 import tensorflow as tf
+# For calculations
 import numpy as np
+# For graphing
 import matplotlib.pyploy as plt
 
-def universal_function_approximator (input_vector, input_dim, hidden_dim, output_dim):
-    with tf.variable_scope ('UniversalApproximator'):
-        weights_hidden_layer = tf.get_variable (name='weights_hidden_layer', shape=[input_dim, hidden_dim],
-            initializer=tf.random_normal_initializer(stddev=0.1))
-        
-        biases_hidden_layer = tf.get_variable (name='biases_hidden_layer', shape=[hidden_dim],
-            initializer=tf.constant_initializer(0.0))
-        
-        z = tf.matmul (input_vector, weights_hidden_layer) + biases_hidden_layer
-        activated_function = tf.nn.relu (z)
-        
-        weights_output_layer = tf.get_variable (name='weights_output_layer', shape=[hidden_dim, output_dim],
-            initializer=tf.random_normal_initializer(stddev=0.1))
-            
-        z = tf.matmul (activated_function, weights_output_layer)
-        
-        return z
-        
+#LOCAL IMPORTS
+from NeuralNetworkFunctions import universal_function_approximator_one_hidden_layer as ua
+
+#############################
+#                           #
+# FUNCTIONS                 #
+#                           #
+#############################
+# FUNCTION_TO_APPROXIMATE
 def function_to_approximate (x):  
     return tf.sin (x)
     
