8e61aeed6a8507dec55d718c24c1980d129925de
Julie Butler <butler@frib.msu.edu>
2019-02-23 12:21:01 -0500

    added comments

--- Code/Neural Networks/NeuralNetworkFunctions.py
+++ Code/Neural Networks/NeuralNetworkFunctions.py
@@ -6,17 +6,48 @@
 #
 # A collection of functions for setting up and running neural networks.  Many are 
 # related to the use of neural networks as universal function approximators.
+
+# FUNCTIONS:
+# initilize_neural_network:  Creates the weights and biases of a neural network, for any
+#   number of hidden layers given in an inputted array of seed values for the weights and
+#   biases.
+# xavier_initialization: initializes the weights of a neural network using the Xavier
+#   initialization method
+# universal_function_approximator: approximates a function of any input and output 
+#   dimensions using a one hidden layer neural network with a specified number of 
+#   neurons.
 ########################################################################################
 
+#############################
+#                           #
+# IMPORTS                   #
+#                           #
+#############################
+# THIRD-PARTY IMPORTS
+# For machine learning
 import tensorflow as tf
+# For matrix calculations
 import numpy as np
 
 def initialize_neural_network (layers):
+    """
+        Inputs:
+            layers (an array of ints): Seed values for the Xavier Initialization.  There
+                should be one number for each layer in the neural network, including the
+                input layer
+        Returns:
+            weights (an array): The weights for each layer in the neural network
+            biases (an array): The biases for each layer in the neural network
+        Calculated the weights and biases for a neural network with one or more hidden 
+        layers using the Xavier initialization method.  
+    """
     weights = []
     biases = []
 
     number_of_layers = len(layers)
 
+    # Calculates weights and biases for each hidden layer and the outout.  len(weights) 
+    # is one less than len(layers) (no weights or biases for the input layer)
     for l in range (0, number_of_layers-1):
         w = xavier_initilization(layers[l], layers[l+1])
         b = tf.Variable (tf.zeros([1, layers[l+1]]))
@@ -27,6 +58,15 @@ def initialize_neural_network (layers):
     return weights, biases
 
 def xavier_initilization (layer_in, layer_out):
+    """
+        Inputs:
+            layer_in (an int): seed value for the Xavier initialization
+            layer_out (an int): seed value for the Xavier initialization
+        Returns: 
+            Unnamed (Tensorflow variable): the weight for the layer
+        Calculates the weights for a layer in a neural network using the Xavier
+        initialization method.
+    """
     xavier_standard_deviation = np.sqrt(2/(layer_in + layer_out))
 
     return tf.Variable (tf.truncated_normal([layer_in, layer_out], 
@@ -51,6 +91,7 @@ def universal_function_approximator_one_hidden_layer (input_vector, input_dim, h
             output_dim (an int): the number of variables outputted by approximating the 
                 function.  Generally will be one.
     """
+    # Makes sure variables are not redefined
     with tf.variable_scope ('UniversalApproximator'):
         weights_hidden_layer = tf.get_variable (name='weights_hidden_layer', 
             shape=[input_dim, hidden_dim],
@@ -67,6 +108,7 @@ def universal_function_approximator_one_hidden_layer (input_vector, input_dim, h
             shape=[hidden_dim, output_dim],
             initializer=tf.random_normal_initializer(stddev=0.1))
             
+        # See notebook #1 page ______
         z = tf.matmul (activated_function, weights_output_layer)
         
         return z
